2329f046519a4a16ba2309d1ddb4d6b857c3a1e9
crtahlin <crt.ahlin@gmail.com>
2023-05-09 20:28:51 +0200

    initial commit

---
+++ R/convert_batch_depth_to_bytes.R
@@ -0,0 +1,18 @@
+#' Convert postage batch depth to bytes
+#'
+#' Function converts postage batch depth into more human readable units of byte multiples.
+#'
+#' @returns Returns value in specified multiples of bytes.
+#'
+#' @param depth Depth of the postage batch.
+#' @param scale_in Scale to convert to - "GB" or "TB" for Gigabytes and Terabytes.
+#'
+#' @export
+convert_batch_depth_to_bytes <- function (depth, scale_in = "GB") {
+  # TODO check if depth is a valid whole number
+  if (scale_in == "GB") {factor <- 1024*1024*1024}
+  if (scale_in == "TB") {factor <- 1024*1024*1024*1024}
+
+  storage <- 2^(depth)*4096 / (factor)
+  return(storage)
+}
---
+++ R/load_global_stamp_data.R
@@ -0,0 +1,15 @@
+#' Load global stamp data from a Bee endpoint
+#'
+#' Function to load global stamp data from local Bee, debug API; prerequisite: a running local Bee.
+#'
+#' @returns Returns a data frame with data about stamps.
+#' @importFrom httr GET
+#' @importFrom jsonlite parse_json
+#' @importFrom httr content
+#'
+#' @export
+load_global_stamp_data <- function () {
+  stamp_data <- GET(url = "http://localhost:1635/batches")
+  stamp_data <- parse_json(content(stamp_data, as = "text"), simplifyVector = TRUE)
+  return(stamp_data$batches)
+}
