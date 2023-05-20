8072834fb372a71de92421418355767fc23380f2
crtahlin <crt.ahlin@gmail.com>
2023-05-20 15:47:18 +0200

    stub

---
+++ R/bee_reserve_size.R
@@ -0,0 +1,5 @@
+#' Get Bee reserve size (relevant for a full node)
+#'
+# TODO light nodes do not have info about reserveSize, so this would have to be gained from Swarmscan via gathering info from several nodes and averaging
+# actually, reserve size even on a light node can be gound at: http://localhost:1635/status/peers
+# for various other nodes and then can be averaged ...
