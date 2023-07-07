455fa824eae23f754ba45cec5fe8e9c7ab98385e
crtahlin <crt.ahlin@gmail.com>
2023-07-07 10:08:32 +0200

    add function that loads swarmscan data about nodes

---
+++ R/load_swarmscan_data.R
@@ -0,0 +1,12 @@
+#' Load swarmscan data
+#'
+#' Loads swarmscan.io data about nodes on the network, from the "https://api.swarmscan.io/v1/network/dump" endpoint.
+#'
+#' @returns Returns a data frame parsed from the JSON response.
+#'
+#' @export
+load_swarmscan_data <- function( ) {
+  swarmscan_dump <- GET(url = "https://api.swarmscan.io/v1/network/dump")
+  swarmscan_data <- parse_json(content(swarmscan_dump, as = "text"), simplifyVector = TRUE)
+  return(swarmscan_data)
+}
